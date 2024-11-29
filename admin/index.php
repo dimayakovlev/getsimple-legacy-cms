597aefb961a7e14e714c760c16a07d2c72fb2794
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2024-11-29 11:53:05 +0300

    Add labels to login and reset password forms

--- admin/index.php
+++ admin/index.php
@@ -2,9 +2,9 @@
 /**
  * Login
  *
- * Allows access to the GetSimple control panel
+ * Allows access to the GetSimple Legacy control panel
  *
- * @package GetSimple
+ * @package GetSimple Legacy
  * @subpackage Login
  */
 
@@ -15,7 +15,7 @@ $load['plugin'] = true;
 // wrap all include and header output in output buffering to prevent sending before headers.
 ob_start();
 	include('inc/common.php');
-	get_template('header', cl($SITENAME).' &raquo; '.i18n_r('LOGIN')); 
+	get_template('header', cl($SITENAME) . ' &raquo; ' . i18n_r('LOGIN'));
 ob_end_flush();
 
 ?>
@@ -26,16 +26,16 @@ ob_end_flush();
 <?php include('template/error_checking.php'); ?>
 <div class="bodycontent clearfix">
 	<div id="maincontent">
-		<div class="main" >
+		<div class="main">
 			<h3><?php echo cl($SITENAME); ?></h3>
 			<?php exec_action('index-login'); ?>
 			<form class="login" action="<?php echo myself(false) . (isset($_SERVER['QUERY_STRING']) ? '?'. htmlentities($_SERVER['QUERY_STRING'], ENT_QUOTES) : ''); ?>" method="post">
-				<p><b><?php i18n('USERNAME'); ?>:</b><br /><input type="text" class="text" id="userid" name="userid" /></p>
-				<p><b><?php i18n('PASSWORD'); ?>:</b><br /><input type="password" class="text" id="pwd" name="pwd" /></p>
+				<p><label for="userid"><?php i18n('USERNAME'); ?>:</label><input type="text" class="text" id="userid" name="userid" /></p>
+				<p><label for="pwd"><?php i18n('PASSWORD'); ?>:</label><input type="password" class="text" id="pwd" name="pwd" /></p>
 				<p><input type="submit" name="submitted" class="submit" value="<?php i18n('LOGIN'); ?>" /></p>
 			</form>
-			<p class="cta" ><b>&laquo;</b> <a href="<?php echo $SITEURL; ?>"><?php i18n('BACK_TO_WEBSITE'); ?></a> &nbsp; | &nbsp; <a href="resetpassword.php"><?php i18n('FORGOT_PWD'); ?></a> &raquo;</p>
-			<div class="reqs" ><?php exec_action('login-reqs'); ?></div>
+			<p class="cta"><b>&laquo;</b> <a href="<?php echo $SITEURL; ?>"><?php i18n('BACK_TO_WEBSITE'); ?></a> &nbsp; | &nbsp; <a href="resetpassword.php"><?php i18n('FORGOT_PWD'); ?></a> &raquo;</p>
+			<div class="reqs"><?php exec_action('login-reqs'); ?></div>
 		</div>
 	</div>
 </div>
--- admin/resetpassword.php
+++ admin/resetpassword.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 /**
  * Reset Password
  *
- * Resets the password for GetSimple control panel access
+ * Resets the password for GetSimple Legacy control panel access
  *
- * @package GetSimple
+ * @package GetSimple Legacy
  * @subpackage Login
  */
 
@@ -12,99 +12,98 @@
 $load['plugin'] = true;
 include('inc/common.php');
 
-if(isset($_POST['submitted'])){
+if (isset($_POST['submitted'])) {
 	
 	// check for csrf
-	if (!defined('GSNOCSRF') || (GSNOCSRF == FALSE) ) {
+	if (!defined('GSNOCSRF') || (GSNOCSRF == FALSE)) {
 		$nonce = $_POST['nonce'];
-		if(!check_nonce($nonce, "reset_password")) {
+		if (!check_nonce($nonce, "reset_password")) {
 			die("CSRF detected!");
 		}
 	}
-	
-	$randSleep = rand(250000,2000000); // random sleep for .25 to 2 seconds
 
-	if(isset($_POST['username']) and !empty($_POST['username']))	{
+	$randSleep = rand(250000, 2000000); // random sleep for .25 to 2 seconds
+
+	if (isset($_POST['username']) and !empty($_POST['username'])) {
 
 		# user filename
-		$file = _id($_POST['username']).'.xml';
-		
+		$file = _id($_POST['username']) . '.xml';
+
 		# get user information from existing XML file
-		
-		if (filepath_is_safe(GSUSERSPATH . $file,GSUSERSPATH)) {
+
+		if (filepath_is_safe(GSUSERSPATH . $file, GSUSERSPATH)) {
 			$data = simplexml_load_file(GSUSERSPATH . $file);
 			$USR = strtolower($data->USR);
 			$EMAIL = $data->EMAIL;
-			
-			if(strtolower($_POST['username']) == $USR) {
+
+			if (strtolower($_POST['username']) == $USR) {
 				# create new random password
 				$random = createRandomPassword();
 				// $random = '1234';
-				
+
 				# create backup
 				createBak($file, GSUSERSPATH, GSBACKUSERSPATH);
-				
+
 				# create password change trigger file
-				$flagfile = GSUSERSPATH . _id($USR).".xml.reset";
+				$flagfile = GSUSERSPATH . _id($USR) . '.xml.reset';
 				copy(GSUSERSPATH . $file, $flagfile);
-				
+
 				# change password and resave xml file
-				$data->PWD = passhash($random); 
+				$data->PWD = passhash($random);
 				$status = XMLsave($data, GSUSERSPATH . $file);
-				
+
 				# send the email with the new password
-				$subject = GSNAME .' '. i18n_r('RESET_PASSWORD') .' '. i18n_r('ATTEMPT');
-				$message = "<p>". cl($SITENAME) ." ". i18n_r('RESET_PASSWORD') ." ". i18n_r('ATTEMPT').'</p>';
-				$message .= "<p>". i18n_r('LABEL_USERNAME').": <strong>". $USR."</strong>";
-				$message .= "<br>". i18n_r('NEW_PASSWORD').": <strong>". $random."</strong>";
-				$message .= '<br>'. i18n_r('EMAIL_LOGIN') .': <a href="'.$SITEURL . $GSADMIN.'/">'.$SITEURL . $GSADMIN.'/</a></p>';
+				$subject = GSNAME . ' ' . i18n_r('RESET_PASSWORD') . ' ' . i18n_r('ATTEMPT');
+				$message = '<p>' . cl($SITENAME) . ' ' . i18n_r('RESET_PASSWORD') . ' ' . i18n_r('ATTEMPT') . '</p>';
+				$message .= '<p>' . i18n_r('LABEL_USERNAME') . ': <strong>' . $USR . '</strong>';
+				$message .= '<br>' . i18n_r('NEW_PASSWORD') . ': <strong>' . $random . '</strong>';
+				$message .= '<br>' . i18n_r('EMAIL_LOGIN') . ': <a href="' . $SITEURL . $GSADMIN . '/">' . $SITEURL . $GSADMIN . '/</a></p>';
 				exec_action('resetpw-success');
-				$status = sendmail($EMAIL,$subject,$message);
+				$status = sendmail($EMAIL, $subject, $message);
 				# show the result of the reset attempt
-				usleep($randSleep); 
+				usleep($randSleep);
 				$status = 'success';
-				redirect("resetpassword.php?upd=pwd-".$status);
+				redirect("resetpassword.php?upd=pwd-" . $status);
 			} else{
 				# username doesnt match listed xml username
 				exec_action('resetpw-error');
 				usleep($randSleep);
 				redirect("resetpassword.php?upd=pwd-success");
-			} 
+			}
 		} else {
 			# no user exists for this username, but do not show this to the submitter
 			usleep($randSleep);
 			redirect("resetpassword.php?upd=pwd-success");
 		}
 	} else {
-		
 		# no username was submitted
 		redirect("resetpassword.php?upd=pwd-error");
 	}
-} 
+}
 
-get_template('header', cl($SITENAME).' &raquo; '.i18n_r('RESET_PASSWORD')); 
+get_template('header', cl($SITENAME) . ' &raquo; ' . i18n_r('RESET_PASSWORD')); 
 
 ?>
 </div>
 </div>
 <div class="wrapper clearfix">
-	
+
 	<?php include('template/error_checking.php'); ?>
-	
+
 	<div id="maincontent">
-		<div class="main" >
-		
+		<div class="main">
+
 		<h3><?php i18n('RESET_PASSWORD'); ?></h3>
 		<p class="desc"><?php i18n('MSG_PLEASE_EMAIL'); ?></p>
-		
-		<form class="login" action="<?php myself(); ?>" method="post" >
+
+		<form class="login" action="<?php myself(); ?>" method="post">
 			<input name="nonce" id="nonce" type="hidden" value="<?php echo get_nonce("reset_password");?>"/>
-			<p><b><?php i18n('LABEL_USERNAME'); ?>:</b><br /><input class="text" name="username" type="text" value="" /></p>
+			<p><label for="username"><?php i18n('LABEL_USERNAME'); ?>:</label><input class="text" name="username" type="text" value="" /></p>
 			<p><input class="submit" type="submit" name="submitted" value="<?php echo i18n('SEND_NEW_PWD'); ?>" /></p>
 		</form>
-		<p class="cta" ><b>&laquo;</b> <a href="<?php echo $SITEURL; ?>"><?php i18n('BACK_TO_WEBSITE'); ?></a> &nbsp; | &nbsp; <a href="index.php"><?php i18n('CONTROL_PANEL'); ?></a> &raquo;</p>
+		<p class="cta"><b>&laquo;</b> <a href="<?php echo $SITEURL; ?>"><?php i18n('BACK_TO_WEBSITE'); ?></a> &nbsp; | &nbsp; <a href="index.php"><?php i18n('CONTROL_PANEL'); ?></a> &raquo;</p>
 		</div>
-		
+
 	</div>
 
 <?php get_template('footer'); ?>

ff711a9d9c5bc9dfc24ccbc33b2d9e466f7db15f
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2024-12-01 14:45:48 +0300

    Update markup of the footer

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -7,41 +7,33 @@
 
 ?>
 		<div id="footer">
-      		<div class="footer-left">
-      		<?php 
-	  		if (cookie_check()) {
-	  			echo '<p><a href="pages.php">'.i18n_r('PAGE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.i18n_r('FILE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.i18n_r('THEME_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.i18n_r('BAK_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.i18n_r('PLUGINS_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.i18n_r('GENERAL_SETTINGS').'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.i18n_r('SUPPORT').'</a></p>';
-	  		}
-      		
-      		if(!isAuthPage()){ ?>
-	      		<p><?php i18n('POWERED_BY'); ?> <a href="<?php echo GSURL; ?>" target="_blank"><?php echo GSNAME; ?></a><?php echo ' &ndash; ' . i18n_r('VERSION') . ' ' . GSVERSION; ?></p>
-      		</div> <!-- end .footer-left -->
-	      	<div class="gslogo">
-		      	<a href="http://get-simple.info/" target="_blank"><img src="template/images/getsimple_logo.gif" alt="GetSimple Content Management System" /></a>
-		    </div>
-	      	<div class="clear"></div>
-	      	<?php
-		      		get_scripts_backend(TRUE);
-		      		exec_action('footer');
-	      		}
-	      	?>
-
+<?php if (!isAuthPage()) { ?>
+			<div class="wrapper clearfix">
+<?php } ?>
+<?php
+	if (cookie_check()) {
+		echo '<p><a href="pages.php">'.i18n_r('PAGE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.i18n_r('FILE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.i18n_r('THEME_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.i18n_r('BAK_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.i18n_r('PLUGINS_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.i18n_r('GENERAL_SETTINGS').'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.i18n_r('SUPPORT').'</a></p>';
+	}
+	if (!isAuthPage()) { ?>
+				<p><?php i18n('POWERED_BY'); ?> <a href="<?php echo GSURL; ?>" target="_blank"><?php echo GSNAME; ?></a><?php echo ' &ndash; ' . i18n_r('VERSION') . ' ' . GSVERSION; ?></p>
+			</div><!-- end .wrapper -->
+<?php
+		get_scripts_backend(true);
+		exec_action('footer');
+	}
+?>
 		</div><!-- end #footer -->
-		<?php 
-		if(!isAuthPage()) {
-			if (isDebug()){
-				global $GS_debug;
-				echo '<h2>'.i18n_r('DEBUG_CONSOLE').'</h2><div id="gsdebug">';
-				echo '<pre>';
-				foreach ($GS_debug as $log){
-					if(is_array($log)) print_r($log).'<br/>';
-					else print($log.'<br/>');
-				}
-				echo '</pre>';	
-				echo '</div>';
-			}
+<?php
+	if (!isAuthPage() && isDebug()) {
+		global $GS_debug;
+		echo '<div><h2>' . i18n_r('DEBUG_CONSOLE') . '</h2><div id="gsdebug"><pre>';
+		foreach ($GS_debug as $log) {
+			if (is_array($log)) print_r($log) . '<br/>';
+			else print($log . '<br/>');
 		}
-		?>
+		echo '</pre></div></div>';
+	}
+?>
 	</div><!-- end .wrapper -->
 </body>
 </html>

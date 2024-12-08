7de8898f6ad632a6c4978a91a79fae7b5c054b69
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2024-12-08 21:15:22 +0300

    Change the naming scheme for upload copies
    Add the postfix `-copy` to the filename of the first copy, and add a number starting with 1 for the next copies.
    Example: filename.txt > filename-copy.txt > filename-copy-1.txt > filename-copy-2.txt.

--- admin/upload.php
+++ admin/upload.php
@@ -4,11 +4,11 @@
  *
  * Displays and uploads files to the website
  *
- * @package GetSimple
+ * @package GetSimple Legacy
  * @subpackage Files
  * @todo Remove relative paths
  */
- 
+
 // Setup inclusions
 $load['plugin'] = true;
 include('inc/common.php');
@@ -44,27 +44,31 @@ if (isset($_FILES['file'])) {
 		if ($_FILES["file"]["error"][$i] > 0)	{
 			$errors[] = i18n_r('ERROR_UPLOAD');
 		} else {
-			
+
 			//set variables
-			$count = '1';
+			$count = 0;
 			$file = $_FILES["file"]["name"][$i];
 
-			$extension = pathinfo($file,PATHINFO_EXTENSION);
-			if(getDef('GSUPLOADSLC',true)) $extension = lowercase($extension);
-	  		$name      = pathinfo($file,PATHINFO_FILENAME);
+			$extension = pathinfo($file, PATHINFO_EXTENSION);
+			if(getDef('GSUPLOADSLC', true)) $extension = lowercase($extension);
+	  		$name      = pathinfo($file, PATHINFO_FILENAME);
 			$name = doTransliteration($name);
 			$name      = clean_img_name(to7bit($name));
 			$base      = $name . '.' . $extension;
 
 			$file_loc = $path . $base;
-			
+
 			//prevent overwriting
-			while ( file_exists($file_loc) ) {
-				$file_loc = $path . $count.'-'. $base;
-				$base = $count.'-'. $base;
-				$count++;
+			while (file_exists($file_loc)) {
+				if ($count > 0) {
+					$file_loc = $path . $name . '-copy-' . $count . '.' . $extension;
+					$base = $name . '-copy-' . $count . '.' . $extension;
+				} else {
+					$file_loc = $path . $name . '-copy.' . $extension;
+					$base = $name . '-copy.' . $extension;
+				}
 			}
-			
+
 			//validate file
 			if (validate_safe_file($_FILES["file"]["tmp_name"][$i], $_FILES["file"]["name"][$i])) {
 				move_uploaded_file($_FILES["file"]["tmp_name"][$i], $file_loc);
@@ -74,28 +78,28 @@ if (isset($_FILES['file'])) {
 					chmod($file_loc, 0644);
 				}
 				exec_action('file-uploaded');
-				
-				// generate thumbnail				
-				require_once('inc/imagemanipulation.php');	
-				genStdThumb($subFolder,$base);					
+
+				// generate thumbnail
+				require_once('inc/imagemanipulation.php');
+				genStdThumb($subFolder, $base);
 				$messages[] = i18n_r('FILE_SUCCESS_MSG').': <a href="'. $SITEURL .'data/uploads/'.$subFolder.$base.'">'. $SITEURL .'data/uploads/'.$subFolder.$base.'</a>';
 			} else {
 				$errors[] = $_FILES["file"]["name"][$i] .' - '.i18n_r('ERROR_UPLOAD');
 			}
-			
+
 			//successfull message
-			
+
 		}
 	 }
 	 // after uploading all files process messages
-		if(sizeof($messages) != 0) { 
+		if (sizeof($messages) != 0) {
 			foreach($messages as $msg) {
-				$success = $msg.'<br />';
+				$success = $msg . '<br />';
 			}
 		}
-		if(sizeof($errors) != 0) {
+		if (sizeof($errors) != 0) {
 			foreach($errors as $msg) {
-				$error = $msg.'<br />';
+				$error = $msg . '<br />';
 			}
 		}
 	}
@@ -106,30 +110,30 @@ if (isset($_GET['newfolder'])) {
 	// check for csrf
 	if (!defined('GSNOCSRF') || (GSNOCSRF == FALSE) ) {
 		$nonce = $_GET['nonce'];
-		if(!check_nonce($nonce, "createfolder")) {
+		if (!check_nonce($nonce, "createfolder")) {
 			die("CSRF detected!");
 		}
 	}
-	
+
 	$newfolder = $_GET['newfolder'];
 	// check for invalid chars
 	$cleanname = doTransliteration(strippath($newfolder));
 	$cleanname = clean_url(to7bit($cleanname, 'UTF-8'));
 	if (file_exists($path.$cleanname) || $cleanname=='') {
-			$error = i18n_r('ERROR_FOLDER_EXISTS');
+		$error = i18n_r('ERROR_FOLDER_EXISTS');
 	} else {
-		if (defined('GSCHMOD')) { 
-			$chmod_value = GSCHMOD; 
+		if (defined('GSCHMOD')) {
+			$chmod_value = GSCHMOD;
 		} else {
 			$chmod_value = 0755;
 		}
 		if (mkdir($path . $cleanname, $chmod_value)) {
 			//create folder for thumbnails
-			$thumbFolder = GSTHUMBNAILPATH.$subFolder.$cleanname;
+			$thumbFolder = GSTHUMBNAILPATH . $subFolder . $cleanname;
 			if (!(file_exists($thumbFolder))) { mkdir($thumbFolder, $chmod_value); }
 			$success = sprintf(i18n_r('FOLDER_CREATED'), $cleanname);
-		}	else { 
-			$error = i18n_r('ERROR_CREATING_FOLDER'); 
+		} else {
+			$error = i18n_r('ERROR_CREATING_FOLDER');
 		}
 	}
 }
@@ -137,12 +141,12 @@ if (isset($_GET['newfolder'])) {
 get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT')); 
 
 ?>
-	
+
 <?php include('template/include-nav.php'); ?>
 
 <div class="bodycontent clearfix">
 	<div id="maincontent">
-		<div class="main" >
+		<div class="main">
 		<h3 class="floated"><?php echo i18n('UPLOADED_FILES'); ?><span id="filetypetoggle">&nbsp;&nbsp;/&nbsp;&nbsp;<?php echo i18n('SHOW_ALL'); ?></span></h3>
 		<div id="file_load">
 		<?php
@@ -152,19 +156,19 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 			$totalsize = 0;
 			$filesArray = array();
       		$dirsArray = array();
-      
+
 			$filenames = getFiles($path);
 
-			if (count($filenames) != 0) { 
+			if (count($filenames) != 0) {
 				foreach ($filenames as $file) {
-					if ($file == "." || $file == ".." || $file == ".htaccess" || $file == "index.php"){
+					if ($file == "." || $file == ".." || $file == ".htaccess" || $file == "index.php") {
             // not a upload file
           	} elseif (is_dir($path . $file)) {
             $dirsArray[$dircount]['name'] = $file;
             clearstatcache();
 						$ss = @stat($path . $file);
 						$dirsArray[$dircount]['date'] = @date('M j, Y',$ss['mtime']);
-            $dircount++;
+						$dircount++;
 					} else {
 						$filesArray[$count]['name'] = $file;
 							$ext = substr($file, strrpos($file, '.') + 1);
@@ -179,18 +183,18 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 					}
 				}
 				$filesSorted = subval_sort($filesArray,'name');
-        $dirsSorted = subval_sort($dirsArray,'name');
+				$dirsSorted = subval_sort($dirsArray,'name');
 			}
-			echo '<div class="edit-nav" >';
+			echo '<div class="edit-nav">';
 			echo '<select id="imageFilter">';
-			echo '<option value="All">'.i18n_r('SHOW_ALL').'</option>';
+			echo '<option value="All">' . i18n_r('SHOW_ALL') . '</option>';
 			if (count($filesSorted) > 0) {
 				foreach ($filesSorted as $filter) {
 					$filterArr[] = $filter['type'];
 				}
-				if (count($filterArr) != 0) { 
+				if (count($filterArr) != 0) {
 					$filterArray = array_unique($filterArr);
-					$filterArray = subval_sort($filterArray,'type');
+					$filterArray = subval_sort($filterArray, 'type');
 					foreach ($filterArray as $type) {
 						
 						# check for image type
@@ -201,33 +205,29 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 							$typeCleaned = $type;
 							$typeCleaned_2 = $type;
 						}
-						 
-						echo '<option value="'.$typeCleaned.'">'.$typeCleaned_2.'</option>';
+						echo '<option value="' . $typeCleaned . '">' . $typeCleaned_2 . '</option>';
 					}
 				}
 			}
 			echo '</select><div class="clear" ></div></div>';
 
-     
-      $pathParts = explode("/",$subPath);
+      $pathParts = explode("/", $subPath);
       $urlPath = null;
-     
+
       echo '<div class="h5 clearfix"><div class="crumbs">/ <a href="upload.php">uploads</a> / ';
 
       foreach ($pathParts as $pathPart){
-	       if ($pathPart!=''){
-	          $urlPath .= $pathPart.'/';
+	       if ($pathPart != '') {
+	          $urlPath .= $pathPart . '/';
 	          
-	          echo '<a href="?path='.$urlPath.'">'.$pathPart.'</a> / ';
+	          echo '<a href="?path=' . $urlPath . '">' . $pathPart . '</a> / ';
 	       }
       }
       echo '</div> <div id="new-folder">
       	<a href="#" id="createfolder">'.i18n_r('CREATE_FOLDER').'</a>
 				<form action="upload.php">&nbsp;<input type="hidden" name="path" value="'.$subPath.'" /><input type="hidden" name="nonce" value="'. get_nonce("createfolder") .'" /><input type="text" class="text" name="newfolder" id="foldername" /> <input type="submit" class="submit" value="'.i18n_r('CREATE_FOLDER').'" />&nbsp; <a href="#" class="cancel">'.i18n_r('CANCEL').'</a></form>
 			</div></div>';
-      
-			
-			
+
      echo '<table class="highlight" id="imageTable">'; 
      echo '<tr><th class="imgthumb" ></th><th>'.i18n_r('FILE_NAME').'</th>';
      echo '<th style="text-align:right;">'.i18n_r('FILE_SIZE').'</th>';
@@ -235,7 +235,7 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
      	 echo '<th style="text-align:right;">'.i18n_r('PERMS').'</th>';
      }
      echo '<th style="text-align:right;">'.i18n_r('DATE').'</th>';
-     echo '<th><!-- actions --></th></tr>';  
+     echo '<th><!-- actions --></th></tr>';
      if (count($dirsSorted) != 0) {
      		$foldercount = 0;
         foreach ($dirsSorted as $upload) {
@@ -247,13 +247,13 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 					}
         	$directory_size = '<span>'.folder_items($path.$upload['name']).' '.i18n_r('ITEMS').'</span>';
         	
-          echo '<tr class="All folder '.$upload['name'].'" >';
-          echo '<td class="imgthumb" ></td><td>';
-        
-          $adm = substr($path . rawurlencode($upload['name']) ,  16); 
+          echo '<tr class="All folder '.$upload['name'].'">';
+          echo '<td class="imgthumb"></td><td>';
+
+          $adm = substr($path . rawurlencode($upload['name']) ,  16);
           echo '<img src="template/images/folder.png" width="11" /> <a href="upload.php?path='.$adm.'" ><strong>'.htmlspecialchars($upload['name']).'</strong></a></td>';
           echo '<td style="width:80px;text-align:right;" ><span>'.$directory_size.'</span></td>';
-          
+
           // get the file permissions.
 		if (isDebug()) {
 			$filePerms = substr(sprintf('%o', fileperms($path.$upload['name'])), -4);
@@ -265,32 +265,32 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 			}
 			echo '<td style="width:70px;text-align:right;"><span>'.$fileOwnerName.'/'.$filePerms.'</span></td>';
 		}
-					
+
 		      echo '<td style="width:85px;text-align:right;" ><span>'. shtDate($upload['date']) .'</span></td>';
           echo '<td class="delete" >'.$directory_delete.'</td>';
           echo '</tr>';
           $foldercount++;
         }
      }
-			if (count($filesSorted) != 0) { 			
+			if (count($filesSorted) != 0) {
 				foreach ($filesSorted as $upload) {
 					$counter++;
-					if ($upload['type'] == i18n_r('IMAGES') .' Images') {
+					if ($upload['type'] == i18n_r('IMAGES') . ' Images') {
 						$cclass = 'iimage';
 					} else {
 						$cclass = '';
 					}
-					echo '<tr class="All '.$upload['type'].' '.$cclass.'" >';
+					echo '<tr class="All '.$upload['type'].' '.$cclass.'">';
 					echo '<td class="imgthumb" >';
-					if ($upload['type'] == i18n_r('IMAGES') .' Images') {
+					if ($upload['type'] == i18n_r('IMAGES') . ' Images') {
 						$gallery = 'rel=" facybox_i"';
 						$pathlink = 'image.php?i='.rawurlencode($upload['name']).'&amp;path='.$subPath;
 						$thumbLink = $urlPath.'thumbsm.'.$upload['name'];
 						$thumbLinkEncoded = $urlPath.'thumbsm.'.rawurlencode($upload['name']);
 						if (file_exists(GSTHUMBNAILPATH.$thumbLink)) {
-							$imgSrc='<img src="../data/thumbs/'. $thumbLinkEncoded .'" />';
+							$imgSrc='<img src="../data/thumbs/'. $thumbLinkEncoded .'">';
 						} else {
-							$imgSrc='<img src="inc/thumb.php?src='. $urlPath . rawurlencode($upload['name']) .'&amp;dest='. $thumbLinkEncoded .'&amp;f=1" />';
+							$imgSrc='<img src="inc/thumb.php?src='. $urlPath . rawurlencode($upload['name']) .'&amp;dest='. $thumbLinkEncoded .'&amp;f=1">';
 						}
 						echo '<a href="'. $path . rawurlencode($upload['name']) .'" title="'. rawurlencode($upload['name']) .'" rel=" facybox_i" >'.$imgSrc.'</a>';
 					} else {
@@ -300,8 +300,7 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 					}
 					echo '</td><td><a title="'.i18n_r('VIEW_FILE').': '. htmlspecialchars($upload['name']) .'" href="'. $pathlink .'" class="primarylink">'.htmlspecialchars($upload['name']) .'</a></td>';
 					echo '<td style="width:80px;text-align:right;" ><span>'. $upload['size'] .'</span></td>';
-             
-		            
+
 					// get the file permissions.
 					if (isDebug()) {
 						$filePerms = substr(sprintf('%o', fileperms($path.$upload['name'])), -4);
@@ -322,7 +321,7 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 			}
 			exec_action('file-extras');
 			echo '</table>';
-			
+
 			if ($counter > 0) { 
 				$sizedesc = '('. fSize($totalsize) .')';
 			} else {
@@ -330,14 +329,14 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('FILE_MANAGEMENT'));
 			}
 			$totalcount = (int)$counter+(int)$foldercount;
 			echo '<p><em><b><span id="pg_counter">'. $totalcount .'</span></b> '.i18n_r('TOTAL_FILES').' '.$sizedesc.'</em></p>';
-		?>	
+		?>
 		</div>
 		</div>
 	</div>
 	
-	<div id="sidebar" >
+	<div id="sidebar">
 	<?php include('template/sidebar-files.php'); ?>
-	</div>	
-	
+	</div>
+
 </div>
 <?php get_template('footer'); ?>
